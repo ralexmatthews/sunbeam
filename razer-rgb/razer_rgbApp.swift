b56baf22830b85d1bed2f1a4468e76a60ee7705b
Alex Matthews <ralexmatthews@gmail.com>
2026-07-23 15:04:11 -0500

    Initial Commit

---
+++ razer-rgb/razer_rgbApp.swift
@@ -0,0 +1,17 @@
+//
+//  razer_rgbApp.swift
+//  razer-rgb
+//
+//  Created by Alex Matthews on 7/23/26.
+//
+
+import SwiftUI
+
+@main
+struct razer_rgbApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
